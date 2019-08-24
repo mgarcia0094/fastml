16779469165ab99f908df374656c3daeb9443109
Miguel Ángel García Gandía <mgarcia0094@gmail.com>
2019-08-24 21:37:23 +0200

    Add sample.go

--- sample.go
+++ sample.go
@@ -5,9 +5,15 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
+func matPrint(X mat.Matrix) {
+ fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+ fmt.Printf("%v\n", fa)
+}
+
 func CreateVector() {
 	u := mat.NewVecDense(3, []float64{1, 2, 3})
-	println("u: ")
+	fmt.Printf("u: ")
 	matPrint(u)
 }
+
 
